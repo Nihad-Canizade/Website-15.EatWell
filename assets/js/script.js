2a7e1f1253a505a8dfb95481e98d0e21560a9233
Nihad-Canizade <nihadcanizade000@gmail.com>
2024-01-22 02:04:25 +0400

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,3 +30,79 @@ function scrollFunction() {
         navbar.style.backgroundColor = "";
     }
 }
+
+
+// Get data Api
+
+let sec4Boxs = document.querySelector(".sec4-boxs");
+let search = document.querySelector("input[type=search]");
+let sort = document.getElementById("sort");
+let info = [];
+
+function getDataApi() {
+    fetch("http://localhost:3000/boxs")
+        .then(response => response.json())
+        .then(data => {
+            sec4Boxs.innerHTML = "";
+            info = info.length ? info : data;
+            info.forEach(element => {
+                sec4Boxs.innerHTML += `
+                <div class="sec4-box">
+                        <div class="sec4-box-icons">
+                            <i class="bi bi-eye"></i>
+                            <i class="bi bi-cart-plus"></i>
+                            <i class="bi bi-heart-fill"></i>
+                        </div>
+                        <div class="sec4-box-forimg">
+                            <img src="${element.image}" alt="Image">
+                        </div>
+                        <div class="sec4-box-fortext">
+                            <p class="sec4-box-p1">${element.name}</p>
+                            <p class="sec4-box-p2">${element.description}</p>
+                            <p class="sec4-box-p3">$${element.price}</p>
+                        </div>
+                    </div>`
+            });
+
+            // Sort Function
+            sort.addEventListener("change", (e) => {
+                if (e.target.value == 'descending') {
+                    info = info.sort((a, b) => b.price - a.price);
+                } else if (e.target.value == 'ascending') {
+                    info = info.sort((a, b) => a.price - b.price);
+                } else {
+                    info = [];
+                }
+                getDataApi();
+            });
+
+            // Search Function
+            search.addEventListener("input", (e) => {
+                let filter = data.filter((el) => {
+                    return el.name.toLocaleLowerCase().startsWith(e.target.value.toLocaleLowerCase());
+                });
+                sec4Boxs.innerHTML = "";
+                filter.forEach(element => {
+                    sec4Boxs.innerHTML += `
+                    <div class="sec4-box">
+                        <div class="sec4-box-icons">
+                            <i class="bi bi-eye"></i>
+                            <i class="bi bi-cart-plus"></i>
+                            <i class="bi bi-heart-fill"></i>
+                        </div>
+                        <div class="sec4-box-forimg">
+                            <img src="${element.image}" alt="Image">
+                        </div>
+                        <div class="sec4-box-fortext">
+                            <p class="sec4-box-p1">${element.name}</p>
+                            <p class="sec4-box-p2">${element.description}</p>
+                            <p class="sec4-box-p3">$${element.price}</p>
+                        </div>
+                    </div>`
+                })
+            })
+        })
+}
+
+getDataApi();
+
